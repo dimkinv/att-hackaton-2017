8ee9e8439cb981752de65764f81ff179ad596406
Danny Vernovsky <dv696w@att.com>
2017-06-20 15:43:34 +0300

    templates

--- app/main-ctrl.js
+++ app/main-ctrl.js
@@ -1,3 +1,14 @@
-angular.module('app', []).controller('mainCtrl', ($scope)=> {
-	$scope.test = 'some test';
+angular.module('app', []).controller('mainCtrl', ($scope) => {
+    $scope.currentTemplate = 'templates/maps.html';
+    
+    $scope.changeScreen = function (screen) {
+        switch (screen) {
+            case 'maps':
+                $scope.currentTemplate = 'templates/maps.html';
+                break;
+            case 'actors':
+                $scope.currentTemplate = 'templates/actors.html'
+                break;
+        }
+    }
 });
