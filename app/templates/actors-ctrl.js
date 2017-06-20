46056348fc30782313c2575103c6eb8ef4fc5fca
dn415q <Qc67!Mma%5566>
2017-06-20 18:59:43 +0300

    show dynamic actors

--- app/templates/actors-ctrl.js
+++ app/templates/actors-ctrl.js
@@ -1,11 +1,23 @@
 angular.module('app').controller('actorsCtrl', ($scope) => {
     $scope.actors = data[0].actors;
+
     let playbackProgress = (param) => {
         param = JSON.parse(param);
-        logger.log(`got event onPlaybackProgress with params ${param}`);
-        logger.log(`milliseconds: ${param['playbackProgressMilliseconds']}`);
-        let progressInSec = param['playbackProgressMilliseconds'] / 1000;
-        logger.log(`progress is: ${progressInSec}`);
+        // logger.log(`milliseconds: ${param['playbackProgressMilliseconds']}`);
+        let progressInMillis = param['playbackProgressMilliseconds'];
+        // logger.log(`progress is: ${progressInMillis}`);
+
+
+        dataHack.forEach((element) => {
+            let actors = element.actors;
+            if (stringTimeToMillis(element.startTime) < progressInMillis && stringTimeToMillis(element.endTime) > progressInMillis) {
+                if (actors ===  $scope.actors) {
+                    return;
+                }
+                logger.log(`${stringTimeToMillis(element.startTime)} < ${progressInMillis} < ${stringTimeToMillis(element.endTime)}`);
+                $scope.actors = actors;
+            }
+        });
     };
 
     emitter.on('onPlaybackProgress', playbackProgress);
